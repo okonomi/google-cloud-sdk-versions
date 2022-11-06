4a8675999df95c505f26a844f57cd9ac94b2ae1f
okonomi <okonomi@oknm.jp>
2022-11-06 16:18:51 +0900

    add convert script

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gem "date"
+gem "json"
+gem "nokogiri"
+
+group :development do
+  gem "debug"
+end
---
+++ release_notes_to_feed.rb
@@ -0,0 +1,73 @@
+require "rubygems"
+require "bundler/setup"
+
+Bundler.require(:default)
+
+
+class SdkRelease
+  attr_accessor :id, :title, :published_at, :contents
+
+  def initialize(id:, title:)
+    self.id = id
+    self.title = title
+    self.published_at = DateTime.parse(SdkRelease.parseTitle(title))
+    self.contents = []
+  end
+
+  def to_feed_item
+    {
+      id: self.id,
+      url: "https://cloud.google.com/sdk/docs/release-notes##{self.id}",
+      title: self.title,
+      content_html: self.content_html,
+      date_published: self.date_published
+    }
+  end
+
+  def content_html
+    contents.map do |content|
+      next if content.content.empty?
+
+      content.to_html
+    end.join
+  end
+
+  def date_published
+    published_at.to_s
+  end
+
+  private
+
+  def self.parseTitle(title)
+    # ex.) 160.0.0 (2017-06-21)
+    m = /(\d+\.\d+\.\d+) \((\d{4}-\d{1,2}-\d{1,2})/.match(title)
+    m[2]
+  end
+end
+
+
+html = $stdin.read
+doc = Nokogiri.HTML(html)
+
+releases = doc.css("article.devsite-article h2").map do |elem|
+  release = SdkRelease.new(id: elem["id"], title: elem.content)
+
+  loop do
+    elem = elem.next_element
+    break if elem.nil? || elem.name == "h2"
+
+    release.contents << elem
+  end
+
+  release
+end
+
+feed = {
+  version: "https://jsonfeed.org/version/1.1",
+  title: "Google Cloud SDK release notes feed",
+  home_page_url: "https://okonomi.github.io/google-cloud-sdk-releases/",
+  feed_url: "https://okonomi.github.io/google-cloud-sdk-releases/feed.json",
+  items: releases.map(&:to_feed_item)
+}
+
+puts feed.to_json
